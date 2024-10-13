5156ff48ebfc34f2708ba847d18ae68a932a8313
Damian Jasiński <dam03jas@gmail.com>
2024-10-13 16:32:41 +0200

    Create zadanie1.cpp

---
+++ zadanie1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class lista {
+    private:
+        vector<int> lista;
+        int currIndex;
+}
+    public:
+        lista() : currIndex(-1) {}
