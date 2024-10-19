1e89f5f3bb9709d2234194b0453959ee9243089e
Damian Jasiński <dam03jas@gmail.com>
2024-10-19 17:20:57 +0200

    metoda dodawania elementu na pocztek listy

--- lista.cpp
+++ lista.cpp
@@ -16,3 +16,17 @@ class lista{
     public:
     lista() : head(nullptr), tail(nullptr), current(nullptr) {} //konstruktor
     }
+
+    //dodaj element na początek lsity
+    void dodajnapoczatek(int element){
+        Node* nowy = new Node(element); //nowy obiekt
+        if(head == nullptr){ // jezeli jest pusta
+            head = nowy;    //lista ma jeden element dlatego ostatni i pierwszy element
+            tail = nowy;    //przypisujemy do tego samego obiektu nowy
+        } else {
+            nowy->next = head; //dla nowego następnym elemtem będzie head
+            head->prev = nowy; //dla head poprzednim elementem będzie nowy 
+            head = nowy;
+        }
+        current = head;
+    } 
