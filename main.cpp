cb34404bd528a69355418b93bdb34046ebeeefa8
Damian Jasiński <dam03jas@gmail.com>
2024-10-19 18:43:09 +0200

    funkcja main do listy dwukierunkowej

--- main.cpp
+++ main.cpp
@@ -1,4 +1,42 @@
 #include <lista.cpp>
+#include <iostream>
+
+int main() {
+    ListaDwukierunkowa lista;
+
+    // Dodaj elementy
+    lista.dodajNaPoczatek(10);
+    lista.dodajNaPoczatek(20);
+    lista.dodajNaKoniec(30);
+    lista.dodajNaIndeks(1, 15);
+
+    // Wyświetl listę
+    cout << "Lista: ";
+    lista.wyswietlListe();
+
+    // Wyświetl listę w odwrotnej kolejności
+    cout << "Lista w odwrotnej kolejności: ";
+    lista.wyswietlListeOdwrotnie();
+
+    // Usuń element z początku i końca
+    lista.usunZPoczatku();
+    lista.usunZKonca();
+
+    // Wyświetl listę po usunięciach
+    cout << "Lista po usunięciu początku i końca: ";
+    lista.wyswietlListe();
+
+    // Przejdź do następnego i poprzedniego elementu
+    lista.wyswietlNastepny();
+    lista.wyswietlPoprzedni();
+
+    // Wyczyszczenie całej listy
+    lista.czyscListe();
+    cout << "Lista po wyczyszczeniu: ";
+    lista.wyswietlListe();
+
+    return 0;
+}
 
 
 
